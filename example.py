6e5af81a0b0af5b214c2d6ecaeec6af6f7a3933f
Aarush321589 <shashikoirala31@gmail.com>
2025-04-17 20:29:32 +0545

    Add files via upload

---
+++ example.py
@@ -0,0 +1,9 @@
+i=13
+if i==13:
+    if i<15:
+        print("i is smaller than 15")  
+    else:
+        print("i is grater than 12 but smaler than 15")
+if i<13:
+    print("i is smaller than 13 too")
+        
